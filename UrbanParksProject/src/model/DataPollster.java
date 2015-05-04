f61fcf5c02ff45e8c974c4d2003225e577a6d2e5
Mike Overby <MikeOverby@outlook.com>
2015-05-03 19:18:39 -0700

    Moved into a model Package
    So, the way I configured this, it didn't actually have the model package
    in the directory structure. Fixed. xD

---
+++ UrbanParksProject/src/model/DataPollster.java
@@ -0,0 +1,149 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//TODO: we definitely need getters for the other classes, like Park and Job.
+public class DataPollster {
+
+	/**
+	 * A reference to a list of jobs in memory.
+	 */
+	List<Job> myJobList;
+	
+	DataPollster(List<Job> theJoblist)
+	{
+		myJobList = theJoblist;
+	}
+	
+	//TODO: test all possible conflicts
+		//Don't have a job they've already signed up for on that day
+		//Job has no room.
+		//TODO:The job's start date has `not passed.
+			//Not sure where we're storing the current date. Do we use the System time?
+	List<Job> getPendingJobs(Volunteer theVolunteer) {
+		//USER STORY 2
+
+		//Called by Volunteer.viewUpcomingJobs()
+
+		//Calls JobList.getCopyList() to get a copy of myJobList
+			//TODO: clear this up with the others.
+			//It already has a reference to the joblist, though?
+		
+		//Check through myJobList and select all Jobs that the Volunteer can sign
+		//up for
+
+		List<Job> applicableJobs = new ArrayList<>();
+		List<Job> volsJobs = getVolunteerJobs(theVolunteer);
+		for (Job j : myJobList)
+		{
+			//TODO, need to review the requirements for signing up for job
+			int i = volsJobs.indexOf(j);
+			if (i != -1) //if volunteer is not signed up for the job already
+			{
+				//Job has no room.
+				if(!j.hasRoom())
+				{
+					continue;
+				}
+				//TODO:The job's start date has not passed.
+				
+				//Test for conflicts with jobs the volunteer has already signed up for.
+				for (Job anyVjob: volsJobs)
+				{
+					//Don't have a job they've already signed up for on that day
+					if (anyVjob.getStartDate().equals(j.getStartDate()) 
+							|| anyVjob.getStartDate().equals(j.getEndDate())
+							|| anyVjob.getEndDate().equals(j.getStartDate())
+							|| anyVjob.getEndDate().equals(j.getEndDate()))
+					{
+						break;
+					}
+				}
+			}
+		}
+		//Make a copy of each selected Job, and put it into a Job List.
+
+		//May or may not hide Jobs sharing the date of a Job that the Volunteer has 
+		//already signed up for
+
+		//Return the list of copied Jobs to the Volunteer
+		return applicableJobs;
+	}
+
+	List<Job> getVolunteerJobs(Volunteer theVolunteer) {
+		//USER STORY 4
+
+		//Called by Volunteer.viewMyJobs()
+
+		//Calls JobList.getCopyList() to get a copy of myJobList
+			//It already has a reference to the joblist
+
+		// Create a new list of Jobs with copies of the Jobs from Schedule’s master 
+		// list
+		List<Job> jobsSignedUpFor = new ArrayList<Job>();
+		
+		//Checks through myJobList and finds all jobs for which the volunteer has
+		// signed up for
+		for (Job j : myJobList)
+		{
+			//TODO: Should I get the job's list via a method?
+			if (j.myVolunteerList.contains(theVolunteer))
+			{
+				jobsSignedUpFor.add(j);
+			}
+		}
+
+		// Return new list of copied Jobs
+		return jobsSignedUpFor;
+	}
+
+	List<Job> getManagerJobs(ParkManager theManager){
+		//USER STORY 5
+
+		//Called by ParkManager.viewUpcomingJobs()
+
+		//Calls JobList.getCopyList() to get a copy of myJobList
+			//It already has a refrence to the joblist
+		
+		//Create a new list of jobs with copies of the Jobs from Schedule’s 
+		//myJobList
+
+		List<Job> jobsManaging = new ArrayList<Job>();
+
+		//Check through myJobList and select all Jobs in all park
+		//managed by the PM.
+		for (Job j : myJobList)
+		{
+			if (theManager.myManagedParks.contains(j.myPark))
+			{
+				jobsManaging.add(j);
+			}
+		}
+		
+		//Return new list of copied Jobs
+		return jobsManaging;
+	}
+
+	List<Volunteer> getVolunteerList(int theJobID) {
+		//USER STORY 6
+		// Called by ParkManager.viewJobVolunteers()
+
+		//Calls JobList.getCopyList() to get a copy of myJobList
+
+		//Create an empty list for returning.
+		List<Volunteer> retVols = new ArrayList<>();
+		// Check through the myJobList and select the Job with that jobID
+		for (Job j : myJobList)
+		{
+			if (j.myJobID == theJobID)
+			{
+				// Use that Job object to get a copied list of associated Volunteers
+				retVols.addAll(j.myVolunteerList);
+			}
+		}
+		
+		// Return that copied list of Volunteers
+		return retVols;
+	}
+}
---
+++ UrbanParksProject/src/model/Job.java
@@ -0,0 +1,166 @@
+package model;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * This class is used to create an instance of a Job.
+ * @author Arsh
+ * @version May 1
+ */
+public class Job {
+	
+	
+	/**
+	 * This value is the identification number of a job.
+	 */
+	public int jobID;
+
+	/**
+	 * This is the list which holds the volunteers that have signed up to participate in this job.
+	 */
+    public List<Volunteer> myVolunteerList;
+
+    /**
+     * NOTE: I don't know what this does but it was in the pseudo-code so I kept it here.
+     */
+    public boolean myIsRequest;
+    
+    /**
+     * This is the start date of the job.
+     */
+    private Date myStartDate;
+
+    /**
+     * This is the ending date of the job.
+     */
+    private Date myEndDate;
+
+    /**
+     * This value holds the maximum number of people who can sign up for the light portion
+     * of this job.
+     */
+    private int myLightMax;
+
+    /**
+     * This value holds the current number of people who have signed for the light portion
+     * of this job.
+     */
+    private int myLightCurrent;
+
+    /**
+     * This value holds the maximum number of people who can sign up for the medium portion
+     * of this job.
+     */
+    private int myMediumMax;
+
+    /**
+     * This value holds the current number of people who have signed for the medium portion
+     * of this job.
+     */
+    private int myMediumCurrent;
+
+    /**
+     * This value holds the maximum number of people who can sign up for the heavy portion
+     * of this job.
+     */
+    private int myHeavyMax;
+
+    /**
+     * This value holds the current number of people who have signed for the heavy portion
+     * of this job.
+     */
+    private int myHeavyCurrent;
+
+    /**
+     * This value holds the park that the job will be located at.
+     */
+    public Park myPark;
+
+    /**
+     * This value holds the manager who created the job.
+     */
+    public ParkManager myManager;
+
+
+    /**
+     * This is a constructor.
+     * @param thePark is the park 
+     * @param theLightSlots is the max number of light positions
+     * @param theMediumSlots is the max number of medium positions
+     * @param theHeavySlots is the max number of heavy positions
+     * @param theStartDate is the starting date of the job
+     * @param theEndDate is the ending date of the job.
+     */
+    public Job(Park thePark, int theLightSlots, int theMediumSlots, int theHeavySlots, Date theStartDate, Date theEndDate) {
+    	myPark = thePark;
+    	myLightMax = theLightSlots;
+    	myMediumMax = theMediumSlots;
+    	myHeavyMax = theHeavySlots;
+    	myStartDate = theStartDate;
+    	myEndDate = theEndDate;
+    }
+    
+    /**
+     * This is used to return the starting date of the job.
+     * @return myStartDate
+     */
+    public Date getStartDate() {
+    	return myStartDate;
+    }
+    
+    /**
+     * This is used to return the ending date of the job.
+     * @return myEndDate
+     */
+    public Date getEndDate() {
+    	return myEndDate;
+    }
+    
+    /**
+     * This method is called when someone has signed up for a light portion of this job.
+     * 
+     * @throws IllegalStateException if called after max has been reached.
+     */
+    public void incrementLight() {
+    	 if (myLightCurrent < myLightMax) {
+    		 myLightCurrent ++;
+    	 } else {
+    		 throw new IllegalStateException("Max has already been reached. Cannot increment.");
+    	 }
+    }
+
+    /**
+     * This method is called when someone has signed up for a medium portion of this job.
+     * 
+     * @throws IllegalStateException if called after max has been reached.
+     */
+    public void incrementMedium() {
+    	if (myMediumCurrent < myMediumMax) {
+    		myMediumCurrent ++;
+    	} else {
+    		throw new IllegalStateException("Max has already been reached. Cannot increment.");
+    	}
+    }
+
+    /**
+     * This method is called when someone has signed up for a heavy portion of this job.
+     * 
+     * @throws IllegalStateException if called after max has been reached.
+     */
+    public void incrementHeavy() {
+    	if (myHeavyCurrent < myHeavyMax) {
+    		myHeavyCurrent ++;
+    	} else {
+    		throw new IllegalStateException("Max has already been reached. Cannot increment.");
+    	}
+    }
+
+    /**
+     * This method is called when someone wants to know if there is room for another volunteer in the job.
+     */
+    public boolean hasRoom()
+    {
+    	return ((myHeavyMax + myMediumMax + myLightMax) - (myHeavyCurrent + myMediumCurrent + myLightCurrent)) > 0;
+    }
+}
