35425fbfa1b40bd38bf37fd0f3f9d398cb5b5295
Asigi <arshsingh_sc@yahoo.com>
2015-05-06 12:30:04 -0700

    Started working on volunteerUI class.
    I've made it so the volunteer can view upcoming jobs.
    Now I'm working on making it so he can sign up for those jobs and also
    see the jobs he's already signed up for.

--- UrbanParksProject/src/model/Volunteer.java
+++ UrbanParksProject/src/model/Volunteer.java
@@ -1,13 +1,29 @@
 package model;
 
 public class Volunteer {
+	
+	
 	private String myFirstName;
 	private String myLastName;
+	
+	private VolunteerUI myUI;
+	private DataPollster myPoll;
+	
+	
 	
 	public Volunteer(String firstName, String lastName)
 	{
 		myFirstName = firstName;
 		myLastName = lastName;
+		
+	}
+	
+	public Volunteer(String firstName, String lastName, DataPollster thePoll) {
+		myFirstName = firstName;
+		myLastName = lastName;
+		
+		myPoll = thePoll;
+		myUI = new VolunteerUI();
 	}
 	
 	public String getFirstName() {
@@ -36,4 +52,86 @@ public class Volunteer {
 	{
 		return myFirstName + " " + myLastName;
 	}
+	
+	
+	
+	//==============stuff I added======================
+	
+	//TODO: Should be removed in favor of having the commands be processed in the UI.
+		public void initialize() {
+			commandLoop();
+		}
+
+		//TODO: Should be removed in favor of having the commands be processed in the UI.
+		/**
+		 * The main loop for Park Manager.<br>
+		 * Lists possible commands, prompts the user for one, and then acts on it.<br>
+		 * If the user chooses to quit, or inputs an invalid command, then the loop terminates.
+		 */
+		public void commandLoop() {
+			System.out.println("Choose an option");
+			myUI.listCommands();
+			String command = myUI.getCommand();
+			
+			if(parseCommand(command)) {
+				commandLoop();
+			}
+		}
+		
+		//TODO: Should be removed in favor of having the commands be processed in the UI.
+		/**
+		 * Parse a command, and call other methods to execute the command.
+		 */
+		public boolean parseCommand(String command) {
+			command = command.toLowerCase(); //lower case to avoid ambiguity
+			
+			switch(command) { 
+				case "view jobs":
+				case "view job":
+				case "view":	
+				case "v":
+				case "1":
+					viewAvailableJobs(); 
+					return true;
+				
+				case "sign up":
+				case "sign":
+				case "s":
+				case "2":
+					signUp();
+					return true;
+					
+				case "view my jobs":
+				case "view my job":
+				case "j":
+				case "m":
+				case "3":
+					viewUpcomingJobs();
+					return true;
+				
+				case "exit":
+				case "close":
+				case "quit":
+				case "4":
+				default: 
+					return false;
+			}
+		}
+	
+	
+	public void viewAvailableJobs() {
+		myUI.displayJobs(myPoll.getAllJobs());
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
