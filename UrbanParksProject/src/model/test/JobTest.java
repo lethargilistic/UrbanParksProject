bfa7512962b8a8e996a7bc8e29949a80760b6da5
Asigi <arshsingh_sc@yahoo.com>
2015-05-06 09:25:48 -0700

    Getting up to date, I guess.

---
+++ UrbanParksProject/src/model/test/JobTest.java
@@ -0,0 +1,112 @@
+package model.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import model.DataPollster;
+import model.Job;
+import model.JobList;
+import model.Park;
+import model.ParkManager;
+import model.Schedule;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class JobTest {
+	
+	private Job myJob;
+	private Park myPark;
+	private ParkManager myParkManager;
+	
+	@Before
+	public void setUp() throws Exception {
+		GregorianCalendar startDate = new GregorianCalendar(6, 11, 1992);
+		GregorianCalendar endDate = new GregorianCalendar(6, 11, 1992);
+		
+		JobList jl = new JobList();
+		Schedule s = new Schedule(jl);
+		DataPollster dp = new DataPollster(jl);
+		
+		Park p = new Park ("Boolean Park", "Tacoma", 19222);
+		List<Park> pList = new ArrayList<>();
+		pList.add(p);
+		
+		myParkManager = new ParkManager(s, dp, pList);
+		
+		myJob = new Job(myPark, 2, 3, 4, startDate, endDate, myParkManager);
+	}
+
+	@Test
+	public void constructorTest() {
+		assertEquals(2, myJob.getLightMax());
+		assertEquals(3, myJob.getMediumMax());
+		assertEquals(4, myJob.getHeavyMax());
+		assertEquals(0, myJob.getLightCurrent());
+		assertEquals(0, myJob.getMediumCurrent());
+		assertEquals(0, myJob.getHeavyCurrent());
+		
+		
+		for (int i = 1; i <= 5; i++)
+		{
+			Job j = new Job(myPark, 2, 3, 4, 
+							new GregorianCalendar(6, 11+i, 1992), 
+							new GregorianCalendar(6, 11+i, 1992), myParkManager);
+			assertEquals(i, j.getJobID());
+		}
+	}
+	
+	@Test
+	public void incrementTest() {
+		myJob.incrementLight();
+		
+		myJob.incrementMedium();
+		myJob.incrementMedium();
+		myJob.incrementMedium();
+		myJob.incrementMedium();
+
+		assertEquals(0, myJob.getHeavyCurrent());
+		assertEquals(4, myJob.getHeavyMax());
+		
+		assertEquals(1, myJob.getLightCurrent());
+		assertEquals(2, myJob.getLightMax());
+		
+		assertEquals(3, myJob.getMediumCurrent());
+		assertEquals(3, myJob.getMediumMax());
+	}
+	
+	@Test
+	public void roomTest() {
+		assertTrue(myJob.hasLightRoom());
+		assertTrue(myJob.hasMediumRoom());
+		assertTrue(myJob.hasHeavyRoom());
+		
+		myJob.incrementLight();
+		myJob.incrementLight();
+		
+		myJob.incrementMedium();
+		myJob.incrementMedium();
+		myJob.incrementMedium();
+		myJob.incrementMedium();
+		
+		myJob.incrementHeavy();
+		myJob.incrementHeavy();
+		
+		assertFalse(myJob.hasLightRoom());
+		assertFalse(myJob.hasMediumRoom());
+		assertTrue(myJob.hasHeavyRoom());
+		assertTrue(myJob.hasRoom());
+		
+		myJob.incrementHeavy();
+		myJob.incrementHeavy();
+		
+		assertFalse(myJob.hasHeavyRoom());
+		assertFalse(myJob.hasRoom());
+	}
+
+}
