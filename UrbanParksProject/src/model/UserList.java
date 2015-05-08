bb10674beb43bfdad003231d9cf0b3f4d1c997ce
Taylor <tjsg1992@gmail.com>
2015-05-08 15:50:39 -0700

    Add a list field for each type of user.

--- UrbanParksProject/src/model/UserList.java
+++ UrbanParksProject/src/model/UserList.java
@@ -3,25 +3,28 @@ package model;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * User List contains a list of all users, including Volunteers, Park Managers, and Administrators. <br>
+ * @author Taylor Gorman
+ * @version 8 May 2015
+ *
+ */
 public class UserList {
-	/**
-	 * This is a list of volunteers who have created accounts with the program.
-	 */
+
+	//User Lists
 	private List<Volunteer> myVolunteerList;
+	private List<ParkManager> myParkManagerList;
+	private List<Administrator> myAdministratorList;
 	
-	
-	
-	
+		
 	public UserList() {
 		myVolunteerList = new ArrayList<Volunteer>();
+		myParkManagerList = new ArrayList<ParkManager>();
+		myAdministratorList = new ArrayList<Administrator>();
 	}
 	
-	/**
-	 * NOTE: we didnt have this on our pseudocode but I thought we should add it so that
-	 * we can keep the volunteer list private.
-	 * 
+	/** 
 	 * This method returns an actual reference to myVolunteerList.
-	 * @return a list.
 	 */
 	public List<Volunteer> getVolunteerList() {
 		return myVolunteerList;
