06fd6d924adf55a0b1b80bd53eb4548766867390
Reid Thompson <rpt265@uw.edu>
2015-05-10 09:45:20 -0700

    made as much progress on AdminUI as possible. Need to change how Jobs store volunteers because I need some way to access their last names in order to do my search... Going to do what I can on Admin now.

--- UrbanParksProject/src/model/AdministratorUI.java
+++ UrbanParksProject/src/model/AdministratorUI.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -10,9 +11,14 @@ import java.util.Scanner;
  */
 public class AdministratorUI {
 	
-	//Ready User Input
+	/**
+	 * A Scanner object to read input for the Administrator's UI.
+	 */
 	private Scanner myIn;
 	
+	/**
+	 * Constructs an AdministratorUI object.
+	 */
 	public AdministratorUI() {
 		myIn = new Scanner(System.in);
 	}
@@ -22,9 +28,12 @@ public class AdministratorUI {
 	 */
 	public void initializeUI() {
 		listCommands();
-		String choice = myIn.nextLine();
-		boolean contSession = parseCommand(choice);
-		
+		String choiceStr = getInput();
+		int choice = getFirstIntChoice(choiceStr);
+		boolean stayLoggedIn = executeOptionChosen(choice);
+		if (stayLoggedIn) { // reprompt user to with list of commands - continuing their interaction with the UI
+			initializeUI();
+		} // the implied else would return control back to whatever method called initializeUI() - probably mainUI
 	}
 	
 	/**
@@ -40,67 +49,138 @@ public class AdministratorUI {
 	}
 	
 	/**
+	 * Returns the integer of the command chosen by the user. If there are more than one
+	 * integer on a line, it takes the first one.
 	 * 
+	 * @param theChoice a string containing the user's response to the list of commands.
+	 * @return the command chosen by the user.
 	 */
-	// getInt here
+	private int getFirstIntChoice(final String theChoice) {
+		final Scanner scan = new Scanner(theChoice);
+		int choice = -1;
+		while (scan.hasNext()) {
+			if (scan.hasNextInt()) {
+				choice = scan.nextInt();
+				break;
+			}
+		}
+		scan.close();
+		return choice;
+	}
 	
 	/**
-	 * Determine what command the user has typed, and call Administrator methods accordingly.
+	 * Calls appropriate Administrator methods if valid input is given. Otherwise, it
+	 * reprompts the user with the original list of commands.
 	 * 
-	 * @param theCommand is the string entered by the Administrator indicating what they
-	 * would like to do within the system.
-	 * @return false if the user wishes to log out and true otherwise.
+	 * @param theChoice is the integer of the option to be executed by the user.
+	 * @return false if the user wishes to log out, and true otherwise.
 	 */
-	public boolean parseCommand(String theCommand) {
-		int choice = -1;
-		final Scanner scan = new Scanner(theCommand);
+	private boolean executeOptionChosen(final int theChoice) {
 		boolean stayLoggedIn = true;
-		
-		// searches string until it encounters a number, which is interpreted
-		// as the desired command of the Administrator
-		while (!scan.hasNextInt()) {
-			scan.next();
-		}
-		choice = scan.nextInt();
-		scan.close();
-		
-		if (choice <= 0) {
+		if (theChoice <= 0 || theChoice > 2) {
 			System.out.println("Invalid command selected. Please try again.\n");
-			listCommands(); // should this call be here...? or would we call it from a different class?
-		} else if (choice == 1) {
-			displayVolunteers();
-		} else if (choice == 2) {
-			logout();
-			stayLoggedIn = false;
-		} else { //
-			System.out.println("Invalid command selected. Please try again.\n");
-			listCommands(); // should this call be here...? or would we call it from a different class?
+			initializeUI(); // restart UI interaction
+		} else { // valid input was given
+			switch(theChoice) { // no default case needed because of original if test
+				case 1: // search volunteers by last name
+				    String lastName = promptForVolsLastName();
+				    boolean lastNameValid = !lastName.equals(""); // other conditions to add?
+					while (!lastNameValid) {
+						System.out.println("No last name was entered. Please try again.\n");
+						lastName = promptForVolsLastName();
+						lastNameValid = !lastName.equals("");
+					} // lastName is valid now
+					// get and output list of Volunteers with matching last names
+					// Step 1) List<Volunteer> matchingVols = getMatchingVolunteers(lastName)
+					// Step 2) displayMatchingVolunteers(matchingVols);
+				case 2: // logout
+					System.out.println("Option 2 selected.\n"
+							+ "Logging out now.\n");
+					stayLoggedIn = false;
+			}
 		}
-		
 		return stayLoggedIn;
 	}
 	
 	/**
-	 * Gets the command last entered by the Administrator indicating what they would like
+	 * Gets the input last entered by the Administrator indicating what they would like
 	 * to do within the system.
 	 * 
 	 * @return a string indicating their command.
 	 */
-	private String getCommand() {	
+	private String getInput() {	
 		return myIn.nextLine();
 	}
 	
 	/**
+	 * Returns a String of the last name of the Volunteer to search for.
 	 * 
+	 * @return a String of the last name of the Volunteer to search for.
 	 */
-	public void displayVolunteers() {
+	private String promptForVolsLastName() {
+		System.out.println("Option 1 selected.\n"
+				+ "Please enter the last name of the Volunteer to search for: \n");
+		return getInput();
+	}
+	
+	/**
+	 * Returns a list of relevant Volunteers based on the last name entered.
+	 * 
+	 * @param theLastName is the last name of the Volunteer to search for.
+	 */
+	private List<Volunteer> getMatchingVolunteers(String theLastName) {
+		return null;
+	}
+	
+	/**
+	 *  Would receive a list of relevant Volunteers.
+	 */
+	private void displayMatchingVolunteers(List<Volunteer> theMatchingVols) {
 		//TODO
 		//VolunteerList = Administrator.searchVolunteers()
 		//Print out list
 	}
-	
-	public void logout() {
-		//TODO
-		//Return control to MainUI
-	}
+
+// OLD METHOD - BROKE IT INTO SMALLER METHODS FOR CODE RE-USE
+//	/**
+//	 * Determine what command the user has typed, and call Administrator methods accordingly.
+//	 * 
+//	 * @param theCommand is the string entered by the Administrator indicating what they
+//	 * would like to do within the system.
+//	 * @return false if the user wishes to log out and true otherwise.
+//	 */
+//	private boolean parseCommand(String theCommand) {
+//		int choice = -1;
+//		final Scanner scan = new Scanner(theCommand);
+//		boolean stayLoggedIn = true;
+//		
+//		// searches string until it encounters a number, which is interpreted
+//		// as the desired command of the Administrator
+//		while (!scan.hasNextInt()) {
+//			scan.next();
+//		}
+//		choice = scan.nextInt();
+//		scan.close();
+//		
+//		if (choice <= 0) {
+//			 // should this call be here...? or would we call it from a different class?
+//		} else if (choice == 1) {
+//			displayVolunteers();
+//		} else if (choice == 2) {
+//			logout();
+//			stayLoggedIn = false;
+//		} else { //
+//			System.out.println("Invalid command selected. Please try again.\n");
+//			listCommands(); // should this call be here...? or would we call it from a different class?
+//		}
+//		
+//		return stayLoggedIn;
+//	}
+
+//	(REID) I DON'T THINK THAT THIS METHOD IS NEEDED. A SIMPLE BOOLEAN CAN HANDLE "LOGGING OUT"
+//  IT CAN DO THIS BY SIMPLY NOT REPROMPTING THE USER AND PRINTING THAT THEY ARE NOW BEING LOGGED OUT.
+//	private void logout() {
+//		//TODO
+//		//Return control to MainUI
+//	}
 }
