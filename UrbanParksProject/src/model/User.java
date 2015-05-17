a135dee2b71d3b55191f9d4851371cbc6b493df1
Reid Thompson <rpt265@uw.edu>
2015-05-17 12:27:47 -0700

    Now the User abstract superclass is added.

---
+++ UrbanParksProject/src/model/User.java
@@ -0,0 +1,57 @@
+package model;
+
+/**
+ * This is a general class for all Users of our Urban Parks program.
+ * 
+ * It applies to Administrators, ParkManagers, and Volunteers.
+ * 
+ * @author Reid Thompson - initial implementation
+ * @version 5.16.2015
+ */
+public abstract class User {
+	private String myFirstName;
+	private String myLastName;
+	private String myEmail;
+	private String myUserType;
+	
+	public User(String theFirstName, String theLastName, String theEmail, String theUserType) {
+		myFirstName = theFirstName;
+		myLastName = theLastName;
+		myEmail = theEmail;
+		myUserType = theUserType;
+	}
+	
+	public String getFirstName() {
+		return myFirstName;
+	}
+	
+	public String getLastName() {
+		return myLastName;
+	}
+	
+	public String getEmail() {
+		return myEmail;
+	}
+	
+	public String getUserType() {
+		return myUserType;
+	}
+	
+	// Reid: first and foremost, do we even need setters? All fields should be initially set in the constructor,
+	// Reid: and none of the user stories specify that we have to allow users to change their data in the system.
+	// Reid: b/c of this fact, I'm opting NOT to give access to a "setUserType()" method b/c that data shouldn't change.
+	// Reid: if we keep these setters, maybe we could return a boolean indicating status of set operation.
+	// Reid: ie. if the caller passes in an empty string/null, etc. etc.
+	
+	public void setFirstName(String theFirstName) {
+		myFirstName = theFirstName;
+	}
+	
+	public void setLastName(String theLastName) {
+		myLastName = theLastName;
+	}
+	
+	public void setEmail(String theEmail) {
+		myEmail = theEmail;
+	}
+}
