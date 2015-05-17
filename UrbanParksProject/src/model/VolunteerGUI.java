73eda1eb1945250351cffa402db8e7b88a4729f7
Asigi <arshsingh_sc@yahoo.com>
2015-05-17 14:28:52 -0700

    Just some basic stuff for volunteer GUI.
    I just commited because I felt like it.

---
+++ UrbanParksProject/src/model/VolunteerGUI.java
@@ -0,0 +1,69 @@
+package model;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+/**
+ * This class is the gui for the volunteer.
+ * Note: it does no logic work.
+ * @author Arsh
+ *
+ */
+public class VolunteerGUI extends JFrame{
+
+	/**
+	 * This is an instance of a volunteer. It is used when a volunteer logs in.
+	 */
+	private Volunteer myVol;
+	
+	/**
+	 * This is a text area .
+	 */
+	private JTextArea myTextArea;// similar to JScrollPane but with less functionality.
+	
+	
+	
+	/**
+	 * This is the constructor.
+	 */
+	public VolunteerGUI(Volunteer theVol) {
+		super("Volunteer");
+		
+		myVol = theVol;
+		startUp();
+		
+	}
+
+
+	/**
+	 * This makes the good stuff appear.
+	 */
+	private void startUp() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+
+
+
+	}
+
+	
+
+
+	//NOTE: create a volunteer class method which gets info from data pollster using getJobListCopy()
+	//then list the jobs on the screen that are in the future.
+
+
+
+
+
+
+
+
+
+
+
+	
+	
+	
+}
