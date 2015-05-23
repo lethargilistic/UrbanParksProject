00f1c922b63b12dc0610e38631a5dc6d5d2f5d2b
Mike Overby <MikeOverby@outlook.com>
2015-05-23 12:46:22 -0700

    Cleaned up AdminUI

--- UrbanParksProject/src/model/Administrator.java
+++ UrbanParksProject/src/model/Administrator.java
@@ -15,195 +15,66 @@ import java.util.List;
  * @version 8 May 2015
  */
 public class Administrator extends User {
-	
-	// this field MUST GO!
-	private AdministratorUI myUI;
-	
+
 	private DataPollster myPollster = DataPollster.getInstance();
-	
+
 	/**
 	 * Constructs an Administrator object.
 	 * 
-	 * @param theFirstName The first name of the Administrator.
-	 * @param theLastName The last name of the Administrator.
-	 * @param theEmail The email of the Administrator.
+	 * @param theFirstName
+	 *            The first name of the Administrator.
+	 * @param theLastName
+	 *            The last name of the Administrator.
+	 * @param theEmail
+	 *            The email of the Administrator.
 	 */
-	public Administrator(String theFirstName, String theLastName, String theEmail) {
+	public Administrator(String theFirstName, String theLastName,
+							String theEmail) {
 		super(theFirstName, theLastName, theEmail);
 		super.setUserType("Administrator");
-		this.myUI = new AdministratorUI();
 	}
-	
-	// this work will be done by GUI
-	
-	/**
-	 * Calls appropriate methods if valid input is given. Otherwise, it
-	 * reprompts the user with the original list of commands.
-	 * 
-	 * @param theChoice is the integer of the option to be executed by the user.
-	 * @return false if the user wishes to log out, and true otherwise.
-	 */
-	private boolean executeOptionChosen(final int theChoice) {
-		boolean stayLoggedIn = true;
-		if (theChoice <= 0 || theChoice > 3) {
-			System.out.println("Invalid command selected. Please try again.\n");
-//			initialize(); // restart UI interaction
-		} else { // valid input was given
-			switch(theChoice) { // no default case needed because of original if test
-				case 1: // list all volunteers by last name, first name (sorted ascending)
-					
-					List<User> allVols = myPollster.getVolunteerListCopy();
 
-					Collections.sort(allVols, new Comparator<User>() {
-						
-						// to sort Volunteers by last name ascending and then by first name ascending
-						@Override
-						public int compare(User theVol1, User theVol2) {
-							int result = theVol1.getLastName().compareTo(theVol2.getLastName());
-							if (result == 0) {
-								result = theVol1.getFirstName().compareTo(theVol2.getFirstName());
-							}
-							return result;
-						}
-						
-					});
-//					displayAllVolunteersLNFN(allVols);
-					break;
-				case 2: // search volunteers by last name
-					System.out.println("Option 2 selected.\n");
-				    String lastName = myUI.promptForVolsLastName();
-				    boolean lastNameValid = !lastName.equals(""); // other conditions to add?
-					while (!lastNameValid) {
-						System.out.println("No last name was entered. Please try again.\n");
-						lastName = myUI.promptForVolsLastName();
-						lastNameValid = !lastName.equals("");
-					} // lastName is valid now
-					// get and output list of Volunteers with matching last names
-					List<User> matchingVols = getMatchingVolunteers(lastName);
-					if (!matchingVols.isEmpty()) {
-						Collections.sort(matchingVols, new Comparator<User>() {
-	
-							// to sort Volunteers in ascending order based on first name
-							@Override
-							public int compare(final User theVol1, final User theVol2) {
-								return theVol1.getFirstName().compareTo(theVol2.getFirstName());
-							}
-							
-						});
-						displayMatchingVolunteers(lastName, matchingVols);
-					} else {
-						System.out.println("There were no Volunteers matching the last name " + lastName);
-					}
-					break;
-				case 3: // logout
-					System.out.println("Option 3 selected.\n"
-							+ "Logging out now.\n");
-					stayLoggedIn = false;
-					break;
-			}
-		}
-		return stayLoggedIn;
-	}
-	
-	// this work will be done by GUI
-	
 	/**
 	 * Returns a List of all Volunteers matching the given last name.
 	 * 
-	 * @param theLastName is the last name by which to search for Volunteer matches.
+	 * @param theLastName
+	 *            is the last name by which to search for Volunteer matches.
 	 * @return a List of Volunteers with the same last name as theLastName.
 	 */
-	private List<User> getMatchingVolunteers(String theLastName) {
+	public List<User> getMatchingVolunteers(String theLastName) {
 		List<User> matchingVols = new ArrayList<>();
 
 		List<User> allVols = myPollster.getVolunteerListCopy();
-		
+
 		for (int i = 0; i < allVols.size(); i++) {
 			final User currVol = allVols.get(i);
 			if (currVol.getLastName().equals(theLastName)) {
 				matchingVols.add(currVol);
 			}
 		}
-		
+
 		return matchingVols;
 	}
-	
-	// this work will be done by GUI
-	
-	/**
-	 * Outputs the list of Volunteers with matching last names sorted by first name in ascending order.
-	 * @param theLastName is the last name used for matching.
-	 * @param theMatchingVols is the list of Volunteers with matching last names.
-	 */
-	private void displayMatchingVolunteers(final String theLastName, final List<User> theMatchingVols) {
-		System.out.println("Here are all of the Volunteers whose last name matches " + theLastName);
-		
-		for (int i = 0; i < theMatchingVols.size(); i++) {
-			final User v = theMatchingVols.get(i);
-			System.out.println(v.getFirstName() + " " + v.getLastName());
-			System.out.println("Email: " + v.getEmail());
-			List<Job> jobs = myPollster.getVolunteerJobs((Volunteer) v);
-			if (!jobs.isEmpty()) {
-				System.out.println("Jobs signed up for: ");
-				for (int j = 0; j < jobs.size(); j++) {
-					final Job job = jobs.get(j);
-					System.out.println("\tJob ID #" + job.getJobID());
-				}
-			} else {
-				System.out.println("This volunteer has not signed up for any jobs.\n");
-			}
-		}
-	}
-	
-	/**
-	 * Returns a list of all the Volunteers in the system.
-	 */
-	public String[][] getVolunteersArray() {
-		
-		List<User> volunteer = myPollster.getVolunteerListCopy();
-		
-		String[][] volunteerArray = new String[volunteer.size()][7];
-		int volunteerNumber = 0;
-		
-		for(User v : volunteer) {
-			volunteerArray[volunteerNumber][0] = v.getLastName();
-			volunteerArray[volunteerNumber][1] = v.getFirstName();
-			volunteerArray[volunteerNumber][2] = v.getEmail();
-			
-			volunteerNumber++;
-		}
-		
-		return volunteerArray;
-	}
-	
-	/**
-	 * Return an array of the volunteers that have theLastName.
-	 */
-	public String[][] searchVolunteers(String theLastName)
-	{		
-		// get and output list of Volunteers with matching last names
-		List<User> matchingVols = getMatchingVolunteers(theLastName);
-		if (!matchingVols.isEmpty()) {
-			Collections.sort(matchingVols, new Comparator<User>() {
 
-				// to sort Volunteers in ascending order based on first name
-				@Override
-				public int compare(final User theVol1, final User theVol2) {
-					return theVol1.getFirstName().compareTo(theVol2.getFirstName());
+	public List<User> getAllVolunteersByLNFN() {
+		List<User> allVols = myPollster.getVolunteerListCopy();
+
+		Collections.sort(allVols, new Comparator<User>() {
+
+			// to sort Volunteers by last name ascending and then by first name
+			// ascending
+			@Override
+			public int compare(User theVol1, User theVol2) {
+				int result = theVol1.getLastName().compareTo(
+						theVol2.getLastName());
+				if (result == 0) {
+					result = theVol1.getFirstName().compareTo(
+							theVol2.getFirstName());
 				}
-				
-			});
-		}
-		
-		String[][] theVolunteerInfo = new String[matchingVols.size()][3];
-		
-		for (int i = 0; i < matchingVols.size(); i++)
-		{
-			theVolunteerInfo[i][0] = matchingVols.get(i).getLastName();
-			theVolunteerInfo[i][1] = matchingVols.get(i).getFirstName();
-			theVolunteerInfo[i][2] = matchingVols.get(i).getEmail();
-		}
-		
-		return theVolunteerInfo;
+				return result;
+			}
+
+		});
+		return allVols;
 	}
 }
--- UrbanParksProject/src/model/AdministratorUI.java
+++ UrbanParksProject/src/model/AdministratorUI.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -15,31 +18,61 @@ public class AdministratorUI {
 	 */
 	private Scanner myIn;
 	
-// HOPEFULLY WE CAN CHANGE THE UI CLASSES LATER TO HAVE USER OBJECTS INSTEAD OF THE OPPOSITE WAY
-//	/**
-//	 * An Administrator object to be associated with this UI object.
-//	 */
-//	private Administrator myAdmin;
+	private Administrator myAdmin;
 	
 	/**
 	 * Constructs an AdministratorUI object.
 	 */
-	public AdministratorUI() {
+	public AdministratorUI(Administrator theAdmin) {
 		myIn = new Scanner(System.in);
+		myAdmin = theAdmin;
 	}
 	
-//	/**
-//	 * Controls the whole flow of interaction regarding this UI class and the Administrator class.
-//	 */
-//	public void initializeUI() {
-//		listCommands();
-//		String choiceStr = getInput();
-//		int choice = getFirstIntChoice(choiceStr);
-//		boolean stayLoggedIn = executeOptionChosen(choice);
-//		if (stayLoggedIn) { // reprompt user to with list of commands - continuing their interaction with the UI
-//			initializeUI();
-//		} // the implied else would return control back to whatever method called initializeUI() - probably mainUI
-//	}
+	public void commandLoop()
+	{
+		boolean stayLoggedIn = true;
+		while (stayLoggedIn)
+		{
+			listCommands();
+			int choice = getUserInt();
+			stayLoggedIn = executeOptionChosen(choice);
+		}
+	}
+
+	/**
+	 * Calls appropriate methods if valid input is given. Otherwise, it
+	 * reprompts the user with the original list of commands.
+	 * 
+	 * @param theChoice
+	 *            is the integer of the option to be executed by the user.
+	 * @return false if the user wishes to log out, and true otherwise.
+	 */
+	private boolean executeOptionChosen(final int theChoice) {
+		boolean stayLoggedIn = true;
+		
+		if (theChoice <= 0 || theChoice > 3) {
+			System.out.println("Invalid command selected. Please try again.\n");
+		} 
+		else { // valid input was given
+			switch (theChoice) { // no default case needed because of original
+									// if test
+				case 1: // list all volunteers by last name, first name (sorted
+						// ascending)
+					myAdmin.getAllVolunteersByLNFN();
+					break;
+				case 2: // search volunteers by last name
+					String name = getUserString();
+					searchByLastName(name);
+					break;
+				case 3: // logout
+					System.out.println("Option 3 selected.\n"
+							+ "Logging out now.\n");
+					stayLoggedIn = false;
+					break;
+			}
+		}
+		return stayLoggedIn;
+	}
 	
 	/**
 	 * Prints the commands that the Administrator can select.
@@ -54,25 +87,70 @@ public class AdministratorUI {
 		System.out.println("3) Logout\n");
 	}
 	
-//	/**
-//	 * Returns the integer of the command chosen by the user. If there are more than one
-//	 * integer on a line, it takes the first one.
-//	 * 
-//	 * @param theChoice a string containing the user's response to the list of commands.
-//	 * @return the command chosen by the user.
-//	 */
-//	public int getFirstIntChoice(final String theChoice) {
-//		final Scanner scan = new Scanner(theChoice);
-//		int choice = -1;
-//		while (scan.hasNext()) {
-//			if (scan.hasNextInt()) {
-//				choice = scan.nextInt();
-//				break;
-//			}
-//		}
-//		scan.close();
-//		return choice;
-//	}
+	private List<User> searchByLastName(String theLastName) {
+		System.out.println("Enter Last Name.\n");
+		String lastName = theLastName;
+		boolean lastNameValid = !lastName.equals(""); // other conditions to
+														// add?
+		while (!lastNameValid) {
+			System.out.println("No last name was entered. Please try again.\n");
+			lastName = theLastName;
+			lastNameValid = !lastName.equals("");
+		} // lastName is valid now
+			// get and output list of Volunteers with matching last names
+		List<User> matchingVols = myAdmin.getMatchingVolunteers(lastName);
+		if (!matchingVols.isEmpty()) {
+			Collections.sort(matchingVols, new Comparator<User>() {
+
+				// to sort Volunteers in ascending order based on first name
+				@Override
+				public int compare(final User theVol1, final User theVol2) {
+					return theVol1.getFirstName().compareTo(
+							theVol2.getFirstName());
+				}
+
+			});
+			displayMatchingVolunteers(lastName, matchingVols);
+		} else {
+			System.out
+					.println("There were no Volunteers matching the last name "
+							+ lastName);
+		}
+
+		return matchingVols;
+	}
+	
+	/**
+	 * Outputs the list of Volunteers with matching last names sorted by first
+	 * name in ascending order.
+	 * 
+	 * @param theLastName
+	 *            is the last name used for matching.
+	 * @param theMatchingVols
+	 *            is the list of Volunteers with matching last names.
+	 */
+	private void displayMatchingVolunteers(final String theLastName,
+			final List<User> theMatchingVols) {
+		System.out
+				.println("Here are all of the Volunteers whose last name matches "
+						+ theLastName);
+
+		for (int i = 0; i < theMatchingVols.size(); i++) {
+			final User v = theMatchingVols.get(i);
+			System.out.println(v.getFirstName() + " " + v.getLastName());
+			System.out.println("Email: " + v.getEmail());
+			List<Job> jobs = myAdmin.getVolunteerJobs((Volunteer) v);
+			if (!jobs.isEmpty()) {
+				System.out.println("Jobs signed up for: ");
+				for (int j = 0; j < jobs.size(); j++) {
+					final Job job = jobs.get(j);
+					System.out.println("\tJob ID #" + job.getJobID());
+				}
+			} else {
+				System.out.println("This volunteer has not signed up for any jobs.\n");
+			}
+		}
+	}
 	
 	/**
 	 * 
@@ -105,50 +183,6 @@ public class AdministratorUI {
 		return userInput;
 	}
 	
-//	/**
-//	 * Calls appropriate Administrator methods if valid input is given. Otherwise, it
-//	 * reprompts the user with the original list of commands.
-//	 * 
-//	 * @param theChoice is the integer of the option to be executed by the user.
-//	 * @return false if the user wishes to log out, and true otherwise.
-//	 */
-//	public boolean executeOptionChosen(final int theChoice) {
-//		boolean stayLoggedIn = true;
-//		if (theChoice <= 0 || theChoice > 2) {
-//			System.out.println("Invalid command selected. Please try again.\n");
-//			initializeUI(); // restart UI interaction
-//		} else { // valid input was given
-//			switch(theChoice) { // no default case needed because of original if test
-//				case 1: // search volunteers by last name
-//				    String lastName = promptForVolsLastName();
-//				    boolean lastNameValid = !lastName.equals(""); // other conditions to add?
-//					while (!lastNameValid) {
-//						System.out.println("No last name was entered. Please try again.\n");
-//						lastName = promptForVolsLastName();
-//						lastNameValid = !lastName.equals("");
-//					} // lastName is valid now
-//					// get and output list of Volunteers with matching last names
-//					// Step 1) List<Volunteer> matchingVols = getMatchingVolunteers(lastName)
-//					// Step 2) displayMatchingVolunteers(matchingVols);
-//				case 2: // logout
-//					System.out.println("Option 2 selected.\n"
-//							+ "Logging out now.\n");
-//					stayLoggedIn = false;
-//			}
-//		}
-//		return stayLoggedIn;
-//	}
-	
-//	/**
-//	 * Gets the input last entered by the Administrator indicating what they would like
-//	 * to do within the system.
-//	 * 
-//	 * @return a string indicating their command.
-//	 */
-//	public String getInput() {	
-//		return myIn.nextLine();
-//	}
-	
 	/**
 	 * Returns a String of the last name of the Volunteer to search for.
 	 * 
@@ -177,48 +211,5 @@ public class AdministratorUI {
 //		//TODO
 //		//VolunteerList = Administrator.searchVolunteers()
 //		//Print out list
-//	}
-
-// OLD METHOD - BROKE IT INTO SMALLER METHODS FOR CODE RE-USE
-//	/**
-//	 * Determine what command the user has typed, and call Administrator methods accordingly.
-//	 * 
-//	 * @param theCommand is the string entered by the Administrator indicating what they
-//	 * would like to do within the system.
-//	 * @return false if the user wishes to log out and true otherwise.
-//	 */
-//	private boolean parseCommand(String theCommand) {
-//		int choice = -1;
-//		final Scanner scan = new Scanner(theCommand);
-//		boolean stayLoggedIn = true;
-//		
-//		// searches string until it encounters a number, which is interpreted
-//		// as the desired command of the Administrator
-//		while (!scan.hasNextInt()) {
-//			scan.next();
-//		}
-//		choice = scan.nextInt();
-//		scan.close();
-//		
-//		if (choice <= 0) {
-//			 // should this call be here...? or would we call it from a different class?
-//		} else if (choice == 1) {
-//			displayVolunteers();
-//		} else if (choice == 2) {
-//			logout();
-//			stayLoggedIn = false;
-//		} else { //
-//			System.out.println("Invalid command selected. Please try again.\n");
-//			listCommands(); // should this call be here...? or would we call it from a different class?
-//		}
-//		
-//		return stayLoggedIn;
-//	}
-
-//	(REID) I DON'T THINK THAT THIS METHOD IS NEEDED. A SIMPLE BOOLEAN CAN HANDLE "LOGGING OUT"
-//  IT CAN DO THIS BY SIMPLY NOT REPROMPTING THE USER AND PRINTING THAT THEY ARE NOW BEING LOGGED OUT.
-//	private void logout() {
-//		//TODO
-//		//Return control to MainUI
 //	}
 }
