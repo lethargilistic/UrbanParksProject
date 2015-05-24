319ce4853719787b4a1183b92d5438b8919f925a
Mike Overby <MikeOverby@outlook.com>
2015-05-24 01:05:12 -0700

    Skeleton for AdministratorTest

---
+++ UrbanParksProject/src/tests/AdministratorTest.java
@@ -0,0 +1,24 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AdministratorTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testGetMatchingVolunteers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAllVolunteersByLNFN() {
+		fail("Not yet implemented");
+	}
+
+}
