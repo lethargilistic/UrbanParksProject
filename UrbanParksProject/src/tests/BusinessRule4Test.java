1a56d7335d64e3437979eb47018543009a669c7e
Mike Overby <MikeOverby@outlook.com>
2015-05-24 12:19:16 -0700

    Unneeded imports.

--- UrbanParksProject/src/tests/BusinessRule4Test.java
+++ UrbanParksProject/src/tests/BusinessRule4Test.java
@@ -3,13 +3,13 @@
  */
 package tests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.lang.reflect.MalformedParametersException;
 import java.util.GregorianCalendar;
 
 import model.Job;
-import model.JobList;
 import model.businessRules.BusinessRule4;
 
 import org.junit.Before;
