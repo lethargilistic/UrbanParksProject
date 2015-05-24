d965c2d62c4942ad39699e894466f752521d43ca
Mike Overby <MikeOverby@outlook.com>
2015-05-24 12:18:21 -0700

    The tests change values, so @BeforeClass is inappropriate
    @BeforeClass is run once for all tests, @Before is run at the start of
    each test.

--- UrbanParksProject/src/tests/ParkManagerTest.java
+++ UrbanParksProject/src/tests/ParkManagerTest.java
@@ -1,9 +1,10 @@
 package tests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 
 import model.DataPollster;
@@ -14,16 +15,11 @@ import model.Schedule;
 import model.UserList;
 import model.Volunteer;
 
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ParkManagerTest {
 	
-	static DataPollster myPollster = DataPollster.getInstance();
-	static Schedule mySchedule = Schedule.getInstance();
 	static JobList myJobList;
 	static UserList myUserList;
 	
@@ -41,32 +37,30 @@ public class ParkManagerTest {
 	static Job job2;
 	static Job job3;
 	
-	
-
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+	@Before
+	public void setUp() throws Exception {
 		myJobList = new JobList();
 		myUserList = new UserList();
 		
-		mySchedule.setJobList(myJobList);
-		mySchedule.setUserList(myUserList);
-		myPollster.setJobList(myJobList);
-		myPollster.setUserList(myUserList);
+		Schedule.getInstance().setJobList(myJobList);
+		Schedule.getInstance().setUserList(myUserList);
+		DataPollster.getInstance().setJobList(myJobList);
+		DataPollster.getInstance().setUserList(myUserList);
 		
 		List<String> parkList = new ArrayList<String>();
 		parkList.add("Test Park 1");
 		parkList.add("Test Park 2");
 		parkList.add("Test Park 3");
-		mySchedule.addUser("testmanager@gmail.com", "Test", "Manager", "ParkManager");
-		mySchedule.updateParkList("testmanager@gmail.com", parkList);
+		Schedule.getInstance().addUser("testmanager@gmail.com", "Test", "Manager", "ParkManager");
+		Schedule.getInstance().updateParkList("testmanager@gmail.com", parkList);
 		
-		testManager = myPollster.getParkManager("testmanager@gmail.com");
+		testManager = DataPollster.getInstance().getParkManager("testmanager@gmail.com");
 		
-		mySchedule.addUser("testvolunteer1@gmail.com", "Test1", "Volunteer1", "Volunteer");
-		mySchedule.addUser("testvolunteer2@gmail.com", "Test2", "Volunteer2", "Volunteer");
-		mySchedule.addUser("testvolunteer3@gmail.com", "Test3", "Volunteer3", "Volunteer");
-		mySchedule.addUser("testvolunteer4@gmail.com", "Test4", "Volunteer4", "Volunteer");
-		mySchedule.addUser("testvolunteer5@gmail.com", "Test5", "Volunteer5", "Volunteer");		
+		Schedule.getInstance().addUser("testvolunteer1@gmail.com", "Test1", "Volunteer1", "Volunteer");
+		Schedule.getInstance().addUser("testvolunteer2@gmail.com", "Test2", "Volunteer2", "Volunteer");
+		Schedule.getInstance().addUser("testvolunteer3@gmail.com", "Test3", "Volunteer3", "Volunteer");
+		Schedule.getInstance().addUser("testvolunteer4@gmail.com", "Test4", "Volunteer4", "Volunteer");
+		Schedule.getInstance().addUser("testvolunteer5@gmail.com", "Test5", "Volunteer5", "Volunteer");		
 		
 		ArrayList<String> volunteer1Array = new ArrayList<String>();
 		volunteer1Array.add("testVolunteer1@gmail.com");
@@ -88,28 +82,16 @@ public class ParkManagerTest {
 		job2 = new Job(1, "Test Park 2", 5, 5, 5, "06032015", "06032015", "testmanager@gmail.com", new ArrayList<ArrayList<String>>());
 		job3 = new Job(2, "Test Park 3", 5, 5, 5, "06052015", "06052015", "testmanager@gmail.com", new ArrayList<ArrayList<String>>());
 		
-		mySchedule.receiveJob(job1);
-		mySchedule.receiveJob(job2);
-		mySchedule.receiveJob(job3);
-		mySchedule.addVolunteerToJob(volunteer4Array, 0);
-		mySchedule.addVolunteerToJob(volunteer2Array, 0);
-		mySchedule.addVolunteerToJob(volunteer1Array, 1);
-		mySchedule.addVolunteerToJob(volunteer2Array, 1);
-		mySchedule.addVolunteerToJob(volunteer3Array, 2);
+		Schedule.getInstance().receiveJob(job1);
+		Schedule.getInstance().receiveJob(job2);
+		Schedule.getInstance().receiveJob(job3);
+		Schedule.getInstance().addVolunteerToJob(volunteer4Array, 0);
+		Schedule.getInstance().addVolunteerToJob(volunteer2Array, 0);
+		Schedule.getInstance().addVolunteerToJob(volunteer1Array, 1);
+		Schedule.getInstance().addVolunteerToJob(volunteer2Array, 1);
+		Schedule.getInstance().addVolunteerToJob(volunteer3Array, 2);
 	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
+	
 	@Test
 	public void testParkManager() {
 		List<String> parkList = new ArrayList<String>();
@@ -139,8 +121,8 @@ public class ParkManagerTest {
 		Job testJob = new Job(3, "Test Park 4", 5, 5, 5, "06052015", "06052015", "testmanager@gmail.com", volunteerList);
 		testManager.addJob(testJob);
 		
-		assertEquals(myPollster.getJobCopy(3).getPark(), "Test Park 4");
-		assertEquals(myPollster.getJobCopy(3).getManager(), "testmanager@gmail.com");
+		assertEquals(DataPollster.getInstance().getJobCopy(3).getPark(), "Test Park 4");
+		assertEquals(DataPollster.getInstance().getJobCopy(3).getManager(), "testmanager@gmail.com");
 	}
 
 	@Test
@@ -174,10 +156,10 @@ public class ParkManagerTest {
 		
 		List<String> parkList = new ArrayList<String>();
 		parkList.add("Not Our Park");
-		mySchedule.addUser("nottestmanager@gmail.com", "Not", "Manager", "ParkManager");
-		mySchedule.updateParkList("nottestmanager@gmail.com", parkList);
+		Schedule.getInstance().addUser("nottestmanager@gmail.com", "Not", "Manager", "ParkManager");
+		Schedule.getInstance().updateParkList("nottestmanager@gmail.com", parkList);
 		
-		assertTrue((myPollster.getParkManager("nottestmanager@gmail.com")).getManagedParks().contains("Not Our Park"));
+		assertTrue((DataPollster.getInstance().getParkManager("nottestmanager@gmail.com")).getManagedParks().contains("Not Our Park"));
 		assertFalse(testManager.getManagedParks().contains("Not Our Park"));
 	}
 
@@ -192,14 +174,14 @@ public class ParkManagerTest {
 		
 		assertEquals(testManager.getManagedParks().size(), 4);
 		
-		mySchedule.addUser("nottestmanager@gmail.com", "Not", "Manager", "ParkManager");
+		Schedule.getInstance().addUser("nottestmanager@gmail.com", "Not", "Manager", "ParkManager");
 		
 		List<String> parkList2 = new ArrayList<String>();
 		parkList2.add("Not Our Park");
-		myPollster.getParkManager("nottestmanager@gmail.com").setManagedParks(parkList2);
+		DataPollster.getInstance().getParkManager("nottestmanager@gmail.com").setManagedParks(parkList2);
 		
-		assertTrue(myPollster.getParkManager("nottestmanager@gmail.com").getManagedParks().contains("Not Our Park"));
-		assertFalse(myPollster.getParkManager("testmanager@gmail.com").getManagedParks().contains("Not Our Park"));
+		assertTrue(DataPollster.getInstance().getParkManager("nottestmanager@gmail.com").getManagedParks().contains("Not Our Park"));
+		assertFalse(DataPollster.getInstance().getParkManager("testmanager@gmail.com").getManagedParks().contains("Not Our Park"));
 	}
 
 	@Test
